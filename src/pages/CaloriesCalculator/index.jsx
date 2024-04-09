24f3512e333e5114ff877584e45a13181dad06b9
Bel <cibeleparra@rockemail.com>
2024-04-09 03:14:24 -0300

    style: corrected calorie calculator style

--- src/pages/CaloriesCalculator/index.jsx
+++ src/pages/CaloriesCalculator/index.jsx
@@ -26,7 +26,7 @@ export const CaloriesCalculator = () => {
     const gramsMonoFat = ((GED * 0.20) / 9).toFixed(0);
 
     const macros = {
-        'Proteínas': 'Proteínas de alto valor biológico, são de origem animal e contêm todos os aminoácidos essenciais. Proteínas de baixo valor biológico, são de origem vegetale contém menos aminoácidos essenciais. Vegetarianos devem variar as fontes de proteínas para obter todos os aminoácidos necessários.',
+        'Proteínas': 'Proteínas de alto valor biológico, são de origem animal e contêm todos os aminoácidos essenciais. Proteínas de baixo valor biológico, são de origem vegetal e contém menos aminoácidos essenciais. Vegetarianos devem variar as fontes de proteínas para obter todos os aminoácidos necessários.',
         'Carboidratos': 'Carboidratos simples são rapidamente absorvidos, fornecendo energia rápida, o que pode ser útil para atletas no pré/pós treino. Já os carboidratos complexos são absorvidos mais lentamente, fornecendo energia duradoura, o que é benéfico para a maioria das pessoas no dia a dia.',
         'Gorduras': `As gorduras trans devem ser limitadas a 2g por dia, enquanto as gorduras saturadas não podem ultrapassar ${gramsSaturedFat}g por dia, as poli-insaturadas apesar de fazer parte das gorduras "boas" não deve ultrapassar de ${gramsPolyFat}g por dia. As gorduras monoinsaturadas são saudáveis, mas não devem ultrapassar de ${gramsMonoFat}g por dia`
     };
