cbe3ba2126b62ce85acdc48ae33da89c208d659b
Bel <cibeleparra@rockemail.com>
2024-04-14 17:32:42 -0300

    fix: removed animation

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
 import { HomePage } from '../pages/HomePage';
 import { InfoPage } from '../pages/InfoPage';
 import { CaloriesPage } from '../pages/CaloriesPage';
@@ -9,13 +8,11 @@ import { MacroPage } from '../pages/MacroPage';
 export default () => {
     const location = useLocation();
     return (
-        <AnimatePresence mode='wait'>
-            <Routes location={location} key={location.pathname}>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/calculadora' element={<InfoPage />} />
-                <Route path='/macros' element={<CaloriesPage />} />
-                <Route path='/alimentacao' element={<MacroPage />} />
-            </Routes>
-        </AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/calculadora' element={<InfoPage />} />
+            <Route path='/macros' element={<CaloriesPage />} />
+            <Route path='/alimentacao' element={<MacroPage />} />
+        </Routes>
     );
 };
